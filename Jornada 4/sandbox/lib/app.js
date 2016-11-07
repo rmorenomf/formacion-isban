6368abdb06cbd2b5615b547145042e90255f0254
ruben_moreno_fernandez <ruben.moreno.fernandez.contractor@dev.bbva.com>
2016-11-07 17:50:20 +0100

    Cambios

--- Jornada 4/sandbox/lib/app.js
+++ Jornada 4/sandbox/lib/app.js
@@ -1,22 +1,8 @@
 "use strict";
 
-var _console = require("examples/console");
+var _module = require("module1");
 
-_console.console.log("[*] Demo app.");
+console.log("[*] Demo app.");
 
-function goTo(action) {
-    var xmlhttp;
-    // compatible with IE7+, Firefox, Chrome, Opera, Safari
-    xmlhttp = new XMLHttpRequest();
-    xmlhttp.onreadystatechange = function () {
-        if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-            var element = document.getElementById('code');
-            debugger;
-            (0, _console.console)("Response retrieved");
-            element.innerHTML = xmlhttp.responseText;
-        }
-    };
-    xmlhttp.open("GET", './examples/' + action + '.js', true);
-    xmlhttp.send();
-}
+console.log((0, _module.sum)(1 + 1));
 //# sourceMappingURL=app.js.map
--- Jornada 4/sandbox/src/app.js
+++ Jornada 4/sandbox/src/app.js
@@ -1,19 +1,5 @@
 console.log("[*] Demo app.");
 
-import { console } from "examples/console";
+import { sum } from 'module1';
 
-function goTo(action){
-    var xmlhttp;
-    // compatible with IE7+, Firefox, Chrome, Opera, Safari
-    xmlhttp = new XMLHttpRequest();
-    xmlhttp.onreadystatechange = function(){
-        if (xmlhttp.readyState == 4 && xmlhttp.status == 200){
-            var element = document.getElementById('code');
-            debugger;
-            console("Response retrieved");
-            element.innerHTML = xmlhttp.responseText;            
-        }
-    }
-    xmlhttp.open("GET", './examples/' + action + '.js', true);
-    xmlhttp.send();
-}
+console.log(sum(1+1)); 
