ce796322953bd324c6e707614feacd61d1360f88
Ruben Moreno <rmorenomf@gmail.com>
2016-11-07 17:51:03 +0100

    Cambios

--- Jornada 4/sandbox/src/app.js
+++ Jornada 4/sandbox/src/app.js
@@ -2,18 +2,4 @@ console.log("[*] Demo app.");
 
 import { console } from "examples/console";
 
-function goTo(action){
-    var xmlhttp;
-    // compatible with IE7+, Firefox, Chrome, Opera, Safari
-    xmlhttp = new XMLHttpRequest();
-    xmlhttp.onreadystatechange = function(){
-        if (xmlhttp.readyState == 4 && xmlhttp.status == 200){
-            var element = document.getElementById('code');
-            debugger;
-            console("Response retrieved");
-            element.innerHTML = xmlhttp.responseText;            
-        }
-    }
-    xmlhttp.open("GET", './examples/' + action + '.js', true);
-    xmlhttp.send();
-}
+//console("Response OK");
